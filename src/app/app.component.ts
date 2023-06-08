99555bc1e0057203bbc47600d026a9312abed7b0
brycebyington <legobrick222@gmail.com>
2023-06-08 15:38:15 -0400

    localhost mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import { UploadService } from './upload.service';
 @Component({
   selector: 'app-root',
   template: `
+  <div class="center-container">
+  <h1 style="color: #FFFFFF">BrycePM - MP3 to BPM+</h1>
   <form>
   <div class="upload-container">
-    <label class="form-label">Select an MP3 File:</label>
+    <label class="form-label" style="color: #FFFFFF">Select an MP3 File:</label>
     <div class="custom-file">
       <input type="file" (change)="onFileSelected($event)" class="form-control">
     </div>
@@ -14,7 +16,9 @@ import { UploadService } from './upload.service';
   <div *ngIf="isLoading" class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
   <div *ngIf="bpm$" class="alert alert-primary mt-3">BPM: {{ bpm$ }}</div>
   <div *ngIf="key$" class="alert alert-primary mt-3">Key: {{ key$ }}<span *ngIf="alt_key_info$">, {{alt_key_info$}}</span></div>
+  <div *ngIf="offset$" class="alert alert-primary mt-3">Offset: {{ offset$ }} ms</div>
 </form>
+</div>
 
   `
 })
@@ -23,6 +27,7 @@ export class AppComponent {
   alt_key_info$!: string;
   key$!: string;
   bpm$!: number;
+  offset$!: number;
   isLoading: boolean = false;
 
   constructor(private uploadService: UploadService) { }
@@ -37,6 +42,7 @@ export class AppComponent {
             this.bpm$ = data.bpm;
             this.key$ = data.key;
             this.alt_key_info$ = data.alt_key_info;
+            this.offset$ = data.offset
             this.isLoading = false;
           });
         }, 1000);
